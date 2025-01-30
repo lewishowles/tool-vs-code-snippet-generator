e537efb521ca0c604ead299bb10ef41cd2cdf44f
Lewis Howles <lewishowles@users.noreply.github.com>
2025-01-30 15:17:10 +0000

    Add the remaining default CSS for our starting point

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,5 @@
 </script>
 
 <template>
-	<div class="h-screen flex items-center justify-center">
-		<div class="p-10 max-w-sm w-full flex flex-col items-center justify-center border-double border-4 rounded-lg border-grey-200">
-			<h1 class="text-purple-800 text-5xl mb-4 font-bold">Hello</h1>
-			<p class="text-grey-500">This should be light grey</p>
-			<p class="text-blue-600">and this should be blue</p>
-		</div>
-	</div>
+	Hi
 </template>

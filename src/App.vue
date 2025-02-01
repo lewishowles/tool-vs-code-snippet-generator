99e1c61e57c127d12ea9c6342a16643ed9cf8f46
Lewis Howles <lewishowles@users.noreply.github.com>
2025-02-01 14:38:46 +0000

    Add a minor layout tweak to improve mobile appearance, just in case

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="max-w-xl mx-auto flex flex-col gap-8 py-20">
+	<div class="max-w-xl mx-auto flex flex-col gap-8 px-8 py-12 md:py-20">
 		<div>
 			<h1 class="mb-4">
 				<span class="bg-gradient-to-br from-purple-800 to-pink-800 bg-clip-text text-5xl font-bold text-transparent">Snippet generator</span>
